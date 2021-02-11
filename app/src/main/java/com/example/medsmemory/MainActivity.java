691dd235f17223c6638dfca163b30796600c9da1
sirjak <sirja.kosonen@gmail.com>
2021-02-11 11:56:22 +0200

    New activity and rough layout for adding medication.

--- app/src/main/java/com/example/medsmemory/MainActivity.java
+++ app/src/main/java/com/example/medsmemory/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.medsmemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toAddMedication(View view) {
+        Intent intent = new Intent(this, AddMedication.class);
+        startActivity(intent);
     }
 }
