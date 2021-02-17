922bf6131b1f13f2e67f133575cb3c6677ea1853
sirjak <sirja.kosonen@gmail.com>
2021-02-17 17:12:02 +0200

    Added new layout file toolbar.xml and used it in the current activities. ••• button temporarily takes to activity_settings.xml

--- app/src/main/java/com/example/medsmemory/MainActivity.java
+++ app/src/main/java/com/example/medsmemory/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView title = findViewById(R.id.toolbar_title);
+        title.setText(R.string.text_appname);
     }
 
     public void toAddMedication(View view) {
         Intent intent = new Intent(this, AddMedication.class);
+        startActivity(intent);
+    }
+
+    public void toSettings(View view) {
+        Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
     }
 }
