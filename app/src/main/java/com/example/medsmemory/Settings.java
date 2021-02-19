e63185012d35b5d761241369ab537ac52f375f62
sirjak <sirja.kosonen@gmail.com>
2021-02-19 12:07:58 +0200

    Lisätty "Edit medications" nappi asetuksiin.

--- app/src/main/java/com/example/medsmemory/Settings.java
+++ app/src/main/java/com/example/medsmemory/Settings.java
@@ -2,7 +2,10 @@ package com.example.medsmemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
@@ -14,5 +17,10 @@ public class Settings extends AppCompatActivity {
 
         TextView title = findViewById(R.id.toolbar_title);
         title.setText(R.string.text_settings);
+    }
+
+    public void editMedications(View view){
+        Intent intent = new Intent(this, EditMedication.class);
+        startActivity(intent);
     }
 }
