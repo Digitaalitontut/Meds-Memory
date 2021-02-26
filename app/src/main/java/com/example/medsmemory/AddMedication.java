dd69e44d752729ab13ef3fa8602d0ccbe3df087d
sirjak <sirja.kosonen@gmail.com>
2021-02-26 15:42:29 +0200

    Added date picker.

--- app/src/main/java/com/example/medsmemory/AddMedication.java
+++ app/src/main/java/com/example/medsmemory/AddMedication.java
@@ -2,11 +2,13 @@ package com.example.medsmemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -18,6 +20,7 @@ import java.util.TimeZone;
 public class AddMedication extends AppCompatActivity {
 
     private TextView alarm;
+    private EditText until;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +28,17 @@ public class AddMedication extends AppCompatActivity {
         setContentView(R.layout.activity_add_medication);
 
         alarm = findViewById(R.id.textViewTime);
+        until = findViewById(R.id.editTextDate);
         TextView title = findViewById(R.id.toolbar_title);
         title.setText(R.string.text_add);
     }
 
     // Creates a time picker when clock is clicked. Picked time then will be sent to TextView.
-    public void setAlarm(View view ) {
+    public void setAlarm(View view) {
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int min = calendar.get(Calendar.MINUTE);
-        TimePickerDialog timePickerDialog = new TimePickerDialog(AddMedication.this, R.style.ThemeOverlay_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, R.style.ThemeOverlay_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 Calendar c = Calendar.getInstance();
@@ -49,6 +53,26 @@ public class AddMedication extends AppCompatActivity {
         timePickerDialog.show();
     }
 
+    public void setDate(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, R.style.ThemeOverlay_AppCompat_Dialog, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar c = Calendar.getInstance();
+                c.set(Calendar.YEAR, year);
+                c.set(Calendar.MONTH, month);
+                c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+                String set = format.format(c.getTime());
+                until.setText(set);
+            }
+
+        }, day, month, year);
+        datePickerDialog.show();
+    }
 }
 
 // Napit löytyy themet:
